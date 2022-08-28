977151266500f3f87e366d1f840d9d2dbf2d6642
teraisama <terai.s.samatua@gmail.com>
2022-08-28 14:04:23 +1200

    Professional Portfolio.html

---
+++ Javascript/script.js
@@ -0,0 +1,16 @@
+//console.log('Hello World from an external javacscript file');
+//console.log('i am Teraivao')
+
+/*console.log("i like to code");
+console.log("i like to cook");*/
+
+//const username = "teraivao"
+//onsole.log("user:" + username);
+
+/*const userName = "Sebin Benjamin";
+console.log("The value in the variable userName is ");
+console.log(userName);*/ 
+
+const favColor = "Blue";
+console.log(favColor);
+favColor = "Green";
